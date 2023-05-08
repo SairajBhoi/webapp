97b5b55322c3db51537a8fbad9f7bf2251017b32
Sairaj B <sairaj.m.bhoi@intimetec.com>
2023-05-08 09:09:44 +0530

    web-app Docker file

---
+++ Docker/db.info.php
@@ -0,0 +1,8 @@
+<?php
+
+define('DB_SERVER', 'first-rds.cgyckgniwvyw.ap-south-1.rds.amazonaws.com');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root1234');
+define('DB_DATABASE', 'sample');
+
+?>
