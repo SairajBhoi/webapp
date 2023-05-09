feaeec419418a19b875fe0b2dcb558b733929215
Sairaj B <sairaj.m.bhoi@intimetec.com>
2023-05-09 10:31:57 +0530

    RDS private subnet association

--- module/aws_db_subnet_group/main.tf
+++ module/aws_db_subnet_group/main.tf
@@ -1,6 +1,6 @@
 resource "aws_db_subnet_group" "rdsdb" {
   name       = "rdsdb-subnet-group"
-  subnet_ids = [var.private_id,var.public_id]
+  subnet_ids = [var.private_id]
 
   tags = {
     Name = "rdsdb-subnet-group"
