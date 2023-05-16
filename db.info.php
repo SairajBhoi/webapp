d630b69a681d65133ce6672fcbc5d321f625cbd0
Sairaj B <sairaj.m.bhoi@intimetec.com>
2023-05-16 17:34:03 +0530

    endpoint env

--- db.info.php
+++ db.info.php
@@ -1,8 +1,11 @@
 <?php
 
-define('DB_SERVER', $_ENV['endpoint']);
-define('DB_USERNAME', $_ENV['user']);
-define('DB_PASSWORD', $_ENV['password']);
-define('DB_DATABASE', "mysql-db");
+define('DB_SERVER', getenv('endpoint'));
+define('DB_USERNAME', getenv('user'));
+define('DB_PASSWORD', getenv('password'));
+define('DB_DATABASE', "sample");
 
+var_dump(DB_SERVER);
+var_dump(DB_USERNAME);
+var_dump(DB_PASSWORD);
 ?>
