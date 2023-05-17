c12e37c775efcf50f53839873c0421427c5d571d
Sairaj B <sairaj.m.bhoi@intimetec.com>
2023-05-17 15:33:54 +0530

    config

--- config.py
+++ config.py
@@ -42,7 +42,7 @@
 customhost = "mysql-db.cgyckgniwvyw.ap-south-1.rds.amazonaws.com"
 customuser = "root"
 custompass = "root1234"
-customdb = "projet-app-db"
+customdb = "projet_app_db"
 custombucket = "emp-info-bucket"
 customregion = "ap-south-1"
 
