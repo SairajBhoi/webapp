d72d4c7c1cbd205be46f7b01d2a7b30ec39cc177
Sairaj B <sairaj.m.bhoi@intimetec.com>
2023-05-17 14:34:51 +0530

    config

--- config.py
+++ config.py
@@ -33,12 +33,28 @@ def get_secret():
     secret = get_secret_value_response['SecretString']
 
     # Your code goes here.
-customhost = secret[host]
-customuser = secret[username]
-custompass = secret[password]
-customdb = "projet-app-db"
-custombucket = "emp-info-bucket"
-customregion = "ap-south-1"
 
 
 
+
+
+
+
+
+
+
+
+    # customhost = secret[host]
+    # customuser = secret[username]
+    # custompass = secret[password]
+    # customdb = "projet-app-db"
+    # custombucket = "emp-info-bucket"
+    # customregion = "ap-south-1"
+
+
+    customhost = "mysql-db.cgyckgniwvyw.ap-south-1.rds.amazonaws.com"
+    customuser = "root"
+    custompass = "root1234"
+    customdb = "projet-app-db"
+    custombucket = "emp-info-bucket"
+    customregion = "ap-south-1"
