54824cd2ea97ce867ade202f72862362426e0ec3
Sairaj B <sairaj.m.bhoi@intimetec.com>
2023-05-22 15:52:03 +0530

    config

--- config.py
+++ config.py
@@ -1,7 +1,31 @@
+import boto3
+import configparser
+import json
+import os
 
-customhost   = "mysql-db.cgyckgniwvyw.ap-south-1.rds.amazonaws.com"
-customuser   = "root"
-custompass   = "root1234"
+secret_name = "mysql-db-secret"
+region_name = "ap-south-1"
+config      = configparser.ConfigParser()
+
+config.read(r'/home/ubuntu/.aws/credentials')
+
+profile_name = 'default' 
+access_key   = config.get(profile_name, 'aws_access_key_id')
+secret_key   = config.get(profile_name, 'aws_secret_access_key')
+session      = boto3.session.Session()
+
+client = session.client(
+   aws_access_key_id     = access_key,
+   aws_secret_access_key = secret_key,
+   service_name          = 'secretsmanager',
+   region_name           = region_name
+)
+response     = client.get_secret_value(SecretId=secret_name)
+secret_value = json.loads(response['SecretString'])
+
+customhost   = secret_value["host"]
+customuser   = secret_value["username"]
+custompass   = secret_value["password"]
 customdb     = "projet_app_db"
 custombucket = "emp-info-bucket"
 customregion = "ap-south-1"
