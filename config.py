ec5357555b6a407214690b18effa5df405c8b832
Sairaj B <sairaj.m.bhoi@intimetec.com>
2023-05-22 06:20:01 +0530

    config file

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 customhost = "mysql-db.cgyckgniwvyw.ap-south-1.rds.amazonaws.com"
 customuser = "root"
 custompass = "root1234"
-customdb = "projet-app-db"
+customdb = "projet_app_db"
 custombucket = "emp-info-bucket"
 customregion = "ap-south-1"
